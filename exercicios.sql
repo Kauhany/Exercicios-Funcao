bed59e3046e65ecef6345d670f6bb6952b197728
Kauhany <kauhanyoliveira52@gmail.com>
2023-10-14 19:55:53 -0300

    Exercicio 1-A

---
+++ exercicios.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE funcao;
+
+USE funcao;
+
+DROP TABLE nomes;
+
+-- Exercicio 1 A
+
+CREATE TABLE nomes (
+    nome VARCHAR(50)
+);
+
+INSERT INTO nomes (nome) 
+VALUES ('Roberta'),
+('Roberto'),
+('Maria Clara'),
+('João');
